bb66871aad0ebace65e491cd25109b0654048247
James Rouse <jimmyrs19@gmail.com>
2026-08-28 11:53:58 -0400

    Add manuscript single-cell analysis scripts

---
+++ Post_Treatment_Brain_scRNA.R
@@ -0,0 +1,7 @@
+#!/usr/bin/env Rscript
+
+script_arg <- commandArgs()[grepl("^--file=", commandArgs())]
+project_root <- dirname(normalizePath(sub("^--file=", "", script_arg[[1]])))
+Sys.setenv(ALEX_MANUSCRIPT_PROJECT_ROOT = project_root)
+source(file.path(project_root, "Analysis_Functions.R"))
+run_manuscript_dataset("post_treatment_brain")
